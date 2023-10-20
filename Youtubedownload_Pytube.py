b92014180b3d3e0c2d2e53ec611b831ab9fab8d7
橋本怜 <hashimotoren@hashimotoreinoMacBook-Air.local>
2023-10-20 10:11:36 +0900

    YouTubedownload on the Desktop

--- Youtubedownload_Pytube.py
+++ Youtubedownload_Pytube.py
@@ -1,4 +1,5 @@
 from pytube import YouTube
+import os
 
 yt = str(input("ダウンロードしたい動画のURLを記入してください:"))
-YouTube(yt).streams.get_highest_resolution().download()
+YouTube(yt).streams.get_highest_resolution().download(os.path.expanduser("~/Desktop"))
--- templates/YouTubedownload_pytube.py
+++
@@ -1,7 +0,0 @@
-from pytube import YouTube
-import os
-
-desktop_dir = os.path.expanduser("~/Desktop")
-
-yt = str(input("URLを入力してください"))
-YouTube(yt).streams.get_highest_resolution().download(desktop_dir)
