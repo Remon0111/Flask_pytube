a98d193a6f151ef8a9e4d62c6744a96d83a5c68e
橋本怜 <hashimotoren@ip-10-95-3-52.ap-northeast-1.compute.internal>
2024-01-26 13:35:29 +0900

    update download

--- static/javascript/style.js
+++ static/javascript/style.js
@@ -1,48 +1,71 @@
+const sleep = (millieconds) => {
+    return new Promise(resolve => setTimeout(resolve, millieconds))
+}
+
+const isdownloading = false
 window.onload = function() {
     let downloadExtension = document.getElementById("Extension");
     downloadExtension.onchange = changeCategory;
+    const MP3_input = document.getElementById("MP3URLTextBox");
+    const MP3_download = document.getElementById("MP3download");
+    const ErrorText = document.getElementById("downloadErrorText");
+
+    MP3_input.addEventListener("change", function() {
+        console.log(MP3_input.value)
+
+        if (MP3_input.value.startsWith("https") && MP3_input.value.includes("youtu.be/") || MP3_input.value.startsWith("https") && MP3_input.value.includes("www.youtube.com/") || MP3_input.value.startsWith("https") && MP3_input.value.includes("music.youtube.com/")) {
+            MP3_download.disabled = false;
+            ErrorText.style.display = "none";
+        } else {
+            MP3_download.disabled = true;
+            ErrorText.style.display = "inline";
+        }
+    });
+
+    const MP4_input = document.getElementById("MP4URLTextBox");
+    const MP4_download = document.getElementById("MP4download");
+
+    MP4_input.addEventListener("change", function() {
+        console.log(MP4_input.value)
+        if (MP4_input.value.startsWith("https") && MP4_input.value.includes("youtu.be/") || MP4_input.value.startsWith("https") && MP4_input.value.includes("www.youtube.com/") || MP4_input.value.startsWith("https") && MP4_input.value.includes("music.youtube.com/")) {
+            MP4_download.disabled = false;
+            ErrorText.style.display = "none";
+        } else {
+            MP4_download.disabled = true;
+            ErrorText.style.display = "inline";
+        }
+    });
+
 }
 
 function changeCategory() {
     let downloadExtension = document.getElementById("Extension");
     let selectdownload = document.getElementById("selectdownload");
     let selectURLTextBox = document.getElementById("selectURLTextBox");
-    let selectPathTextBox = document.getElementById("selectPathTextBox");
     let MP4downloadButton = document.getElementById("MP4download");
     let MP4URLTextBox = document.getElementById("MP4URLTextBox");
-    let MP4PathTextBox = document.getElementById("MP4PathTextBox");
     let MP3downloadButton = document.getElementById("MP3download");
     let MP3URLTextBox = document.getElementById("MP3URLTextBox");
-    let MP3pathTextBox = document.getElementById("MP3PathTextBox");
     if (downloadExtension.value == "選択") {
         selectdownload.style.display = "inline";
         selectURLTextBox.style.display = "inline";
-        selectPathTextBox.style.display = "inline";
         MP4downloadButton.style.display = "none";
         MP4URLTextBox.style.display = "none";
-        MP4PathTextBox.style.display = "none";
         MP3downloadButton.style.display = "none";
         MP3URLTextBox.style.display = "none";
-        MP3pathTextBox.style.display = "none";
     }else if (downloadExtension.value == "MP4"){
         selectdownload.style.display = "none";
         selectURLTextBox.style.display = "none";
-        selectPathTextBox.style.display = "none";
         MP4downloadButton.style.display = "inline";
         MP4URLTextBox.style.display = "inline";
-        MP4PathTextBox.style.display = "inline";
         MP3downloadButton.style.display = "none";
         MP3URLTextBox.style.display = "none";
-        MP3pathTextBox.style.display = "none";
     }else if (downloadExtension.value == "MP3"){
         selectdownload.style.display = "none";
         selectURLTextBox.style.display = "none";
-        selectPathTextBox.style.display = "none";
         MP4downloadButton.style.display = "none";
         MP4URLTextBox.style.display = "none";
-        MP4PathTextBox.style.display = "none";
         MP3downloadButton.style.display = "inline";
         MP3URLTextBox.style.display = "inline";
-        MP3pathTextBox.style.display = "inline";
     }
 }
